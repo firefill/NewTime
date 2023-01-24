c7a9813ce0a1f3828e27823512b1381247d6a468
firefill <83161211+firefill@users.noreply.github.com>
2023-01-24 23:55:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ $(".element").each(function () {
     $(".stage").children(".element").not(".active").removeClass("inactive");
   });
 });
+$(function(){
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 30){
+      $("body").addClass("sticky-header");
+    }else{
+      $("body").removeClass("sticky-header");
+    }
+  });
+});
