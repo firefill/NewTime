26f465daa380f5b37b4251042c3e13f5fffd8681
Deiillish <100082110+Deiillish@users.noreply.github.com>
2023-01-25 18:02:08 +0300

    FAQ для толика

---
+++ FAQ/script.js
@@ -0,0 +1,45 @@
+
+FAQimg1.addEventListener('click', function(){
+    FAQimg1.classList.toggle("activeFAQ")
+    if (FAQimg1.classList.contains('activeFAQ')){
+        FAQimg1.style.transform = 'rotate('+ 45 + 'deg)';   
+        FAQli1.style.height = 80+'px'
+    }else{
+        FAQimg1.style.transform = 'rotate('+ 0 + 'deg)';   
+        FAQli1.style.height = 40+'px'
+    }
+
+})
+FAQimg2.addEventListener('click', function(){
+    FAQimg2.classList.toggle("activeFAQ")
+    if (FAQimg2.classList.contains('activeFAQ')){
+        FAQimg2.style.transform = 'rotate('+ 45 + 'deg)';   
+        FAQli2.style.height = 80+'px'
+    }else{
+        FAQimg2.style.transform = 'rotate('+ 0 + 'deg)';   
+        FAQli2.style.height = 40+'px'
+    }
+
+})
+FAQimg3.addEventListener('click', function(){
+    FAQimg3.classList.toggle("activeFAQ")
+    if (FAQimg3.classList.contains('activeFAQ')){
+        FAQimg3.style.transform = 'rotate('+ 45 + 'deg)';   
+        FAQli3.style.height = 80+'px'
+    }else{
+        FAQimg3.style.transform = 'rotate('+ 0 + 'deg)';   
+        FAQli3.style.height = 40+'px'
+    }
+
+})
+FAQimg4.addEventListener('click', function(){
+    FAQimg4.classList.toggle("activeFAQ")
+    if (FAQimg4.classList.contains('activeFAQ')){
+        FAQimg4.style.transform = 'rotate('+ 45 + 'deg)';   
+        FAQli4.style.height = 80+'px'
+    }else{
+        FAQimg4.style.transform = 'rotate('+ 0 + 'deg)';   
+        FAQli4.style.height = 40+'px'
+    }
+
+})
